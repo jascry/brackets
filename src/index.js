34516eb660a88f8e4b5b523e5bcfc1132cc93d7c
Daniel Kopleński <dkoplenski@gmail.com>
2020-03-10 21:11:46 +0100

    Bracket task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let bracketStack = [];
+  let openBracket = {};
+  let closedBracket = {};
+  bracketsConfig.forEach(array => 
+    {openBracket[array[0]] = array[1];
+      closedBracket[array[1]] = true;
+    });
+
+  for(let i = 0; i < str.length; i++) {
+    if(openBracket[str[i]] && closedBracket[str[i]] && !bracketStack.find(x => x === str[i])) {
+      bracketStack.push(str[i]);
+      continue;
+    }
+    if(openBracket[str[i]] && !closedBracket[str[i]]) {
+      bracketStack.push(str[i]);
+    } else if (closedBracket[str[i]]) {
+      if(openBracket[bracketStack.pop()] !== str[i]) {
+        return false;
+      }
+    } else {
+      return false;
+    }
+  }
+  return bracketStack.length === 0;
 }
+
+//console.log(check('(((|||)))', [['[', ']'], ['(', ')'],['|', '|']]))
